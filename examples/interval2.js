252077747669c70fb85b9d1d05c31ee619796f10
Jason Laster <jason.laster.11@gmail.com>
2017-05-24 17:05:38 -0400

    expand interval example

---
+++ examples/interval2.js
@@ -0,0 +1,97 @@
+function oneIIFE() {
+  var ugh = [];
+  var i = 0;
+
+  (function () {
+    (function() {
+     ugh.push(i++);
+     debugger;
+    })()
+  })()
+
+  debugger;
+}
+
+function twoIIFEs() {
+  var ugh = [];
+  var i = 0;
+
+  (function () {
+    (function() {
+     ugh.push(i++);
+     debugger;
+   })();
+
+    (function() {
+     ugh.push(i++);
+     debugger;
+   })();
+
+  })()
+
+
+  debugger;
+}
+
+function twoDeepIIFEs() {
+  var ugh = [];
+  var i = 0;
+
+  (function () {
+    (function() {
+     ugh.push(i++);
+     debugger;
+   })()
+  })();
+
+  (function () {
+    (function() {
+     ugh.push(i++);
+     debugger;
+   })()
+  })();
+
+}
+
+function intervals() {
+  var running = true
+  var arr = [];
+  var i = 0;
+  setInterval(function() {
+    arr.push(i++);
+    if (running) {
+      debugger
+    }
+  },30)
+}
+
+
+function timeouts() {
+  (function() {
+    var arr = [];
+    var i = 0;
+    setTimeout(function() {
+      arr.push(i++);
+      debugger
+    },5)
+
+    setTimeout(function() {
+      arr.push(i++);
+      debugger
+    },15)
+
+  })()
+}
+
+
+function calls() {
+  var ugh = [];
+  var i = 0;
+  function foo() {
+    ugh.push(i++);
+    debugger
+  }
+
+  foo()
+  foo()
+}

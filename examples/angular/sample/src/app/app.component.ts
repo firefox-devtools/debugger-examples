e61f9e2f4d973f042095a5521b074a210c3c07b4
Logan Smyth <loganfsmyth@gmail.com>
2018-03-21 16:23:45 -0700

    A standard angular app build with @angular/cli's 'new'.

---
+++ examples/angular/sample/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
+
+class AnotherThing {
+  prop = 4;
+}
+
+console.log("here", AnotherThing);
